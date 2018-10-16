e587af8f7f103eec0e7241c10bd0c8d2fcb18220
ArtemG96 <44069248+ArtemG96@users.noreply.github.com>
2018-10-16 13:19:49 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <html>
-    <head>Hello world!</head>
+    <head>
+        <title>Hello World!</title>
+    </head>
         <body>
-        <?php echo "Hello world!"?>
+        <?php echo "<p>Hello World</p>";
+        //Параметры для подключения
+        $db_host = "site.local";
+        $db_password ="123";
+        $db_user = "root";
+        
+        
+        //Подключение к базе данных 
+        $db = mysqli_connect ("localhost","root","") OR DIE("Не могу создать соединение ");
+        
+        //Выборка базы
+        mysqli_select_db ( $db, "database2" ) or die ("no database");
+
+        //Установление кодировки
+        mysqli_query( $db, "SET NAMES 'utf8'" );
+        
+        $result = mysqli_query( $db, "SELECT * FROM `test2`" );
+        printf("Select returned %d rows.\n", mysqli_num_rows($result));
+
+        while($myrow = mysqli_fetch_array($result))
+            echo "Артикль - ".$myrow['Article']."<br>";
+            echo "Наименование товара - ".$myrow['producte name']."<br>";
+            echo "Цена - ".$myrow['price']."<br>";
+          ;
+        ?>
         </body>
 </html>
